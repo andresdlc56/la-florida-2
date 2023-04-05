a8e15e6b88830ba12f1d9ea2c9f8256c69e04bbe
Andres de la Cruz <andresdlc56@gmail.com>
2023-04-05 11:03:48 -0400

    Logo florida agregado

--- src/ui/componets/Navbar.jsx
+++ src/ui/componets/Navbar.jsx
@@ -3,14 +3,17 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a className="navbar-brand" href="#">
+                    <img src="/assets/img/LOGO-1.png" alt="Bootstrap" width="120" height="75" />
+                </a>
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ms-auto my-2 my-lg-0">
-                        <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#about">Inicio</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#about">Nostros</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#services">Servicios</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#portfolio">Productos</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#contact">Contacto</a></li>
                     </ul>
                 </div>
             </div>
