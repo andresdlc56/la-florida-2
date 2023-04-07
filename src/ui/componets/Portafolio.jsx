893230f5d57bce9bd55536d68bff5a7dd62d884b
Andres de la Cruz <andresdlc56@gmail.com>
2023-04-06 22:17:35 -0400

    probando img

--- src/ui/componets/Portafolio.jsx
+++ src/ui/componets/Portafolio.jsx
@@ -6,7 +6,7 @@ export const Portafolio = () => {
                 <div className="row g-0">
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
-                            <img className="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
+                            <img className="img-fluid" src="assets/img/portfolio/thumbnails/PRO IMG 1.jpg" alt="..." />
                             <div className="portfolio-box-caption">
                                 <div className="project-category text-white-50">Category</div>
                                 <div className="project-name">Project Name</div>
