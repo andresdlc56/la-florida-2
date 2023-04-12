67ddfb776e146640ef534675c5c61bc35737f39f
Jonathan davila <davilajesus970@gmail.com>
2023-04-12 16:11:30 -0400

    hice cambio en contacto

--- src/ui/componets/Contact.jsx
+++ src/ui/componets/Contact.jsx
@@ -13,7 +13,57 @@ export const Contact = () => {
                 <div className="row gx-4 gx-lg-5 justify-content-center mb-5">
                     <div className="col-lg-6">
                         {/*----------Formulario--------------------*/}
-                        
+                        <section className="page-section" id="Contactanos">
+                            <div className="row gx-4 gx-lg-5 justify-content-center mb-5">
+                                <div className="col-lg-6"></div>
+                                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                                            
+                                        <div className="form-floating mb-3">
+                                                <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                                <label>Nombre completo</label>
+                                                <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                        </div>
+                                            
+                                        <div className="form-floating mb-3">
+                                                <input className="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                                <label>Direccion de correo electronico</label>
+                                                <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                                <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                                        </div>
+                                            
+                                        <div className="form-floating mb-3">
+                                                <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                                                <label>Numero de telefono</label>
+                                                <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                                        </div>
+                                            
+                                        <div className="form-floating mb-3">
+                                                <textarea 
+                                                    className="form-control" 
+                                                    id="message" 
+                                                    type="text"
+                                                >
+                                                </textarea>
+
+                                                <label>Mensaje</label>
+                                                <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                        </div>
+                                            
+                                        <div className="d-none" id="submitSuccessMessage">
+                                                <div className="text-center mb-3">
+                                                    <div className="fw-bolder">Form submission successful!</div>
+                                                    To activate this form, sign up at
+                                                    <br />
+                                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                                </div>
+                                        </div>
+                                            
+                                        <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
+                                            
+                                        <div className="d-grid"><button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Enviar</button></div>
+                                </form>
+                            </div>
+                        </section>
                     </div>
                 </div>
                 <div className="row gx-4 gx-lg-5 justify-content-center">
