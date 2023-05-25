215d12e5897df01d687ec2797a4e500d8e31b1fe
Jonathan <davilajesus970@gmail.com>
2023-05-25 16:22:36 -0400

    Se realizo cambios en redes-sociales

--- src/ui/home/components/Services.jsx
+++ src/ui/home/components/Services.jsx
@@ -3,55 +3,50 @@ export const Services = () => {
     return (
         <section className="page-section" id="services">
             <div className="container px-4 px-lg-5">
-<<<<<<< HEAD:src/ui/componets/Services.jsx
-                <h2 className="text-center mt-0">NUESTRAS REDES SOCIALES</h2>
-=======
-                <h2 className="text-center mt-0">Nuestras Redes</h2>
->>>>>>> Nosotros:src/ui/home/components/Services.jsx
+                <h2 className="text-center mt-0">Visitanos En Nuestras Redes Sociales</h2>
                 <hr className="divider" />
                 <div className="row gx-4 gx-lg-5">
-                    <div className="col-lg-3 col-md-6 text-center">
+                <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-<<<<<<< HEAD:src/ui/componets/Services.jsx
-                            <div className="mb-2"><i className="bi-instagram fs-1 text-primary"></i></div>
-                            <h3 className="h4 mb-2">INSTAGRAM</h3>
-                            <p className="text-muted mb-0">@Licorerialaflorida</p>
-=======
-                            <div className="mb-2"><i className="bi-facebook fs-1 text-primary"></i></div>
-                            <h3 className="h4 mb-2">FACEBOOK</h3>
-                            <p className="text-muted mb-0">Licoreria La Florida, C.A.</p>
->>>>>>> Nosotros:src/ui/home/components/Services.jsx
+                            <a 
+                                href="https://es-es.facebook.com/LaFloridaVenezuela/"
+                                target="_blank"
+                            >
+                                <div className="mb-2"><i className="bi-facebook fs-1 text-primary"></i></div>
+                                <h3 className="h4 mb-2">FACEBOOK</h3>
+                                <p className="text- mb-0">@Difloriente</p>
+                            </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-<<<<<<< HEAD:src/ui/componets/Services.jsx
-                            <div className="mb-2"><i className="bi-facebook fs-1 text-primary"></i></div>
-                            <h3 className="h4 mb-2">FACEBOOK</h3>
-                            <p className="text-muted mb-0">Licoreria La Florida, C.A.</p>
-=======
-                            <div className="mb-2"><i className="bi-instagram fs-1 text-primary"></i></div>
-                            <h3 className="h4 mb-2">INSTAGRAM</h3>
-                            <p className="text-muted mb-0">@licorerialaflorida </p>
->>>>>>> Nosotros:src/ui/home/components/Services.jsx
+                            <a 
+                                href="https://www.instagram.com/licorerialaflorida/?hl=es"
+                                target="_blank"
+                            >
+                                <div className="mb-2"><i className="bi-instagram fs-1 text-primary"></i></div>
+                                <h3 className="h4 mb-2">INSTAGRAM</h3>
+                                <p className="text- mb-0">@Difloriente</p>
+                            </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
-                            <div className="mb-2"><i className="bi-twitter fs-1 text-primary"></i></div>
-                            <h3 className="h4 mb-2">TWITTER</h3>
-<<<<<<< HEAD:src/ui/componets/Services.jsx
-                            <p className="text-muted mb-0">@Diflorienteve</p>
-=======
-                            <p className="text-muted mb-0">@Diflorienteve </p>
->>>>>>> Nosotros:src/ui/home/components/Services.jsx
+                            <a 
+                                href="https://twitter.com/laflorida_ve?lang=es"
+                                target="_blank"
+                            >
+                                <div className="mb-2"><i className="bi-twitter fs-1 text-primary"></i></div>
+                                <h3 className="h4 mb-2">TWITTER</h3>
+                                <p className="text- mb-0">@Difloriente</p>
+                            </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-md-6 text-center">
                         <div className="mt-5">
                             <div className="mb-2"><i className="bi-heart fs-1 text-primary"></i></div>
                             <h3 className="h4 mb-2">SIGUENOS</h3>
-                            <p className="text-muted mb-0">Con tu apoyo nos ayudas mucho</p>
+                            <p className="text- mb-0">No olvides apoyarnos</p>
                         </div>
                     </div>
                 </div>
