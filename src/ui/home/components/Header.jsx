b34cf4e2a1ed5d0a1c3f742ce8159a4ebf5ad6d7
30287561jona <davilajesus970@gmail.com>
2023-06-05 14:46:08 -0400

    se reliazo cambios en Nosotros

--- src/ui/home/components/Header.jsx
+++ src/ui/home/components/Header.jsx
@@ -5,11 +5,11 @@ export const Header = () => {
             <div className="container px-4 px-lg-5 h-100">
                 <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div className="col-lg-8 align-self-end">
-                        <h1 className="text-white font-weight-bold">Your Favorite Place for Free Bootstrap Themes</h1>
+                        <h1 className="text-white font-weight-bold">Bienvenidos</h1>
                         <hr className="divider" />
                     </div>
                     <div className="col-lg-8 align-self-baseline">
-                        <p className="text-white-75 mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
+                        <p className="text-white-75 mb-5">Licorería La Florida, empresa dedicada a la elaboración, envejecimiento y envasado de bebidas alcohólicas</p>
                         <a className="btn btn-primary btn-xl" href="#about">Find Out More</a>
                     </div>
                 </div>
