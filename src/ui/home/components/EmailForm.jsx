718ef8d7ce89d876fb96f7fbf204a9c42602a202
Andres de la Cruz <andresdlc56@gmail.com>
2023-06-06 10:44:10 -0400

    Agregando algo de validacion al form de emails

--- src/ui/home/components/EmailForm.jsx
+++ src/ui/home/components/EmailForm.jsx
@@ -1,33 +1,24 @@
+
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-
-//import { useForm } from "../../../hooks/useForm";
+const initialState = {
+    enviado: false,
+    mensaje: ''
+}
 
 
 export const EmailForm = () => {
 
-    /*const { onInputChange, onResetForm, fullName, email, phoneNumber, description } = useForm({
-        fullName: '',
-        email: '',
-        phoneNumber: '',
-        description: ''
-    });*/
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
+    const [alerta, setAlerta] = useState( initialState );
+
+    const { enviado, mensaje } = alerta;
     
-    //const { register, formState: { errors }, handleSubmit } = useForm();
-    //const onSubmit = (data) => console.log(data);
-
-
-    /*
-    const onSubmitForm = (e) => {
-        e.preventDefault();
-
-        const newMessage = {
-            fullName,
-            email,
-            phoneNumber,
-            description
-        }
+    
+    const onSubmit = (data) => {
+        //console.log(data);
 
         //Enviando la info a la API
         fetch("https://formsubmit.co/ajax/de1d8c0f7d83925a497fa37bdc81bb45", {
@@ -36,32 +27,89 @@ export const EmailForm = () => {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
-            body: JSON.stringify(newMessage)
+            body: JSON.stringify(data)
         })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            setAlerta({
+                enviado: true,
+                mensaje: 'Su mensaje se ha enviado con exito. Pronto lo contactaremos'
+            })
+
+            setTimeout(() => {
+                setAlerta( initialState )
+            }, 3000)
+        })
         .catch(error => console.log(error));
+
+        reset();
     }
-    */
+
+
 
     return (
-        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+        <form onSubmit={ handleSubmit(onSubmit) }>
             <div className="form-floating mb-3">
-                <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                <label htmlFor="name">Full name</label>
+                <input 
+                    className="form-control" 
+                    id="name" 
+                    type="text" 
+                    {...register("name", {
+                        required: true,
+                        maxLength: 50
+                    })} 
+                />
+                
+                <label htmlform="name">Full name</label>
+                
+                {/* Mensaje de error */}
+                { errors.name?.type === 'required' && <p>El campo Full name es Requerido</p> }
+                { errors.name?.type === 'maxLength' && <p>El campo Full name no debe exceder los 50 caracteres</p> }
+
+                {/* Mensaje de error de la plantilla (no funciona) */}
                 <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
             </div>
                             
             <div className="form-floating mb-3">
-                <input className="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                <label htmlForm="email">Email address</label>
+                <input 
+                    className="form-control" 
+                    id="email" 
+                    type="email" 
+                    {...register("email", {
+                        required: true,
+                        pattern: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/
+                    })}
+                />
+                
+                <label htmlform="email">Email address</label>
+                
+                {/* Mensaje de error */}
+                { errors.email?.type === 'required' && <p>El campo Email es Requerido</p> }
+                { errors.email?.type === 'pattern' && <p>El formato del Email es incorrecto</p> }
+
+                {/* Mensaje de error de la plantilla (no funciona) */}
                 <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
                 <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
             </div>
                             
             <div className="form-floating mb-3">
-                <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                <label htmlFor="phone">Phone number</label>
+                <input 
+                    className="form-control" 
+                    id="phone" 
+                    type="tel" 
+                    {...register("phone", {
+                        required: true,
+                        maxLength: 11
+                    })}
+                />
+                
+                <label htmlform="phone">Phone number</label>
+
+                {/* Mensaje de error */}
+                { errors.phone?.type === 'required' && <p>El campo Numero Telefonico name es Requerido</p> }
+                { errors.phone?.type === 'maxLength' && <p>El campo Numero Telefonico no debe exceder los 11 caracteres</p> }
+
+                {/* Mensaje de error de la plantilla (no funciona) */}
                 <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
             </div>
                             
@@ -70,27 +118,47 @@ export const EmailForm = () => {
                     className="form-control" 
                     id="message" 
                     type="text" 
-                    placeholder="Enter your message here..." 
-                    style="height: 10rem" 
-                    data-sb-validations="required">
-                    </textarea>
+                    style={{ height: "10rem" }} 
+                    {...register("message", {
+                        required: true
+                    })}
+                />
                     
-                <label htmlFor="message">Message</label>
+                <label htmlform="message">Message</label>
+
+                {/* Mensaje de error */}
+                { errors.message?.type === 'required' && <p>El campo Message es Requerido</p> }
+
+                {/* Mensaje de error de la plantilla (no funciona) */}
                 <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
             </div>
-                           
-            <div className="d-none" id="submitSuccessMessage">
-                <div className="text-center mb-3">
-                    <div className="fw-bolder">Form submission successful!</div>
-                    To activate this form, sign up at
-                    <br />
-                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                </div>
-            </div>
+
+            {
+                (enviado) 
+                && (
+                    <div  id="submitSuccessMessage">
+                        <div className="text-center mb-3">
+                            <div className="fw-bolder">Form submission successful!</div>
+                            { mensaje }
+                            <br />
+                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                        </div>
+                    </div>
+                )
+            }              
+            
            
             <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
            
-            <div className="d-grid"><button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
+            <div className="d-grid">
+                <button 
+                    className="btn btn-primary btn-xl" 
+                    id="submitButton" 
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </div>
         </form>
     )
 }
